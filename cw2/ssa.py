04abe5fe6643751bb5fcdfd801d36d201885ef4b
Zhalgas <zhalgas99@outlook.com>
2020-04-12 01:50:06 +0600

    minor changes

--- cw2/ssa.py
+++ cw2/ssa.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 def main(data):
     tau = (1 + len(data)) // 4
-    X_T = np.array([data.cost[i:i + tau] for i in range(len(data) - tau + 1)])
+    X_T = np.array(data.cost)[np.arange(tau) + np.arange(len(data) - tau + 1)[:, np.newaxis]]
     cov_mat = np.dot(X_T.T, X_T) / len(data)
     eig_vals, eig_vecs = np.linalg.eig(cov_mat)
     Y = np.dot(eig_vecs.T, X_T.T)
@@ -15,7 +15,7 @@ def main(data):
     X_new = np.dot(eig_vecs[:, :r], Y[:r])
 
     tmp_X = np.fliplr(X_new)
-    cost_smoth = np.array([np.mean(tmp_X.diagonal(tmp_X.shape[1] - 1 - i)) for i in range(len(data))])
+    cost_smooth = np.array([np.mean(tmp_X.diagonal(tmp_X.shape[1] - 1 - i)) for i in range(len(data))])
 
     eig_vecs_ast = eig_vecs[:tau - 1, :r]
     eig_vecs_tau = eig_vecs[-1, :r]
@@ -27,9 +27,9 @@ def main(data):
         x_new = np.dot(np.dot(eig_vecs_tau, eig_vecs_ast.T), Q) / (1 - np.dot(eig_vecs_tau, eig_vecs_tau.T))
         res = np.append(res, x_new)
         Q = np.append(Q, x_new)[1:]
-    return res
+    return res, cost_smooth
 
 
 if __name__ == '__main__':
-    plt.plot(main(pd.read_csv('data.csv', delimiter=';', names=['date', 'cost'])))
+    plt.plot(main(pd.read_csv('data.csv', delimiter=';', names=['date', 'cost']))[0])
     plt.show()
